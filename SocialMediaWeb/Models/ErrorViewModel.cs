7f78b515af204aad3fc8febe77e933c5c9e99b86
Shivam Goel <shivamgoel150@gmail.com>
2023-03-03 11:24:15 +0530

    Add project files.

---
+++ SocialMediaWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SocialMediaWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
