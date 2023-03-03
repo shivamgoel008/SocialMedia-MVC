b9a14fcc861a667113dde2523720b55c606f0b0b
Shivam Goel <shivamgoel150@gmail.com>
2023-03-03 11:24:15 +0530

    controllers added Add project files.

---
+++ SocialMediaWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using SocialMediaWeb.Models;
+namespace SocialMediaWeb.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        // constructor
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options) 
+        {
+        }
+        // categories is the table name
+        // get is getter and set is setter
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ SocialMediaWeb/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SocialMediaWeb.Models
+{
+    public class User
+    {
+        [Key] public int Id { get; set; }
+        [Required] public string userName { get; set; }
+        [Required] public string userEmail { get; set; }
+        [Required] [Range(8,100,ErrorMessage ="Minimum 8 Character Required")] public string  userPassword{ get; set; }
+
+        public DateTime CreateDateTime { get; set; } = DateTime.Now;
+    }
+}
